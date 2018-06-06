9e515fe580185ed724522154281e24bb743bf52b
Emiton <emiton_alves@my.uri.edu>
2018-06-06 23:56:35 +0000

    Improved the controller so that the proper amount of funds are removed from the user's account when they purchase a given amount of stocks

--- public/angular-app/buy/buy-controller.js
+++ public/angular-app/buy/buy-controller.js
@@ -3,6 +3,21 @@ angular.module('cdfinance').controller("BuyController", BuyController);
 function BuyController($http, $window, AuthFactory, jwtHelper, $location) {
   var vm = this;
   
+  // This function updates the user balance by removing the funds used to purchase stocks
+  // Had to write separate function in order to get data from HTTP requests which are ASYNC
+  function updateUserAccountBalance(currentStockPrice, user, amountPurchased) {
+        // Cannot send a var of type number, must create an object to send to database
+        var newData = {"amount" : currentStockPrice * amountPurchased * -1}
+        // The put request will access the user account and change the balance
+        $http.put('/api/users/'+ user +"/deposit", newData).then(function(response) {
+        if (response.status == 200) {
+          console.log("SUCCESS")
+        }
+      }).catch(function(error) {
+        console.log(error);
+      })
+      }
+  
   vm.buy = function() {
     if ($window.sessionStorage.token && AuthFactory.isLoggedIn) {
       var token = $window.sessionStorage.token;
@@ -11,11 +26,27 @@ function BuyController($http, $window, AuthFactory, jwtHelper, $location) {
       
       var data = {"symbol" : vm.symbol, "amount": vm.amount}
       
+      // Post purchased stock to user's portfolio
       $http.post('/api/users/'+ username +"/stocks", data).then(function(response) {
         //check the responses
       }).catch(function(error) {
         console.log(error);
       })
+      
+      // Get stock price, taken from find-controller.js
+      $http.get("/api/stocks/" + vm.symbol).then(function(response) {
+      console.log("found stock")
+      var stockprice = response.data.price
+      vm.stockprice = stockprice;
+      // Once stockprice is found, make call to update user balance  
+      updateUserAccountBalance(stockprice, username, vm.amount);
+      
+      }).catch(function(error) {
+        if (error) {
+          vm.error = error;
+        }
+      })
+    
     } else {
       $location.path('/');
     }
