bdfa729ba35fa53b94e210c549ae15ee05346c6b
Kyle Mayers <kyle7150@gmail.com>
2018-06-06 03:16:39 +0000

    able to display purchased stocks on dashboard and display total balance and amount added to funds when depositing

--- public/angular-app/deposit/deposit-controller.js
+++ public/angular-app/deposit/deposit-controller.js
@@ -9,10 +9,18 @@ function DepositController($http, $window, AuthFactory, jwtHelper, $location) {
       var decodedToken = jwtHelper.decodeToken(token);
       var username = decodedToken.username;
       var data = {"amount" : vm.amount};
+      var totalamount = $http.get('/api/users/' + username).then(function(response) {
+      vm.balance = response.data;});
+      
+      
+      $http.get('/api/users/' + username).then(function(response) {
+      vm.balance = response.data;
+      });
       
       $http.put('/api/users/'+ username +"/deposit", data).then(function(response) {
         if (response.status == 200) {
-          vm.message = "Balance increased."
+          vm.balance += vm.amount;
+          vm.message = "$" + vm.amount + " added to account. Total Balance = " + vm.balance; 
         }
       }).catch(function(error) {
         console.log(error);
